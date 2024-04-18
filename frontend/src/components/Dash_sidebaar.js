bc21f6b6dcffeb7149e0e9d53cccefe109a65449
arpit2804 <guptaji1004@gmail.com>
2024-04-18 10:11:30 +0530

    dashboard navbar and sidebaar

---
+++ frontend/src/components/Dash_sidebaar.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import image from '../Assets/user_photo.jpeg'
+
+const Sidebar = () => {
+  return (
+    <div className="bg-[#f6ebf8] fixed top-32 left-14  py-2 flex flex-col justify-center items-center shadow-xl z-10 text-xl border border-purple-300 border-opacity-45 rounded-2xl">
+      <div>
+        <ul>
+          <li className="py-3 border-b border-purple-300 flex flex-col justify-center items-center">
+            <div className=' w-24 object-contain my-3 '>
+             <img src={image} className='rounded-full'></img>
+             </div>
+            <a href="#" className="text-purple-700  flex font-bold text-[26px] items-center px-12 hover:px-7">
+              Czleen
+            </a>
+            <div className=' text-sm my-1 text-purple-500'>czleen1234@gmail.com</div>
+          </li>
+          <li className="py-3 border-b border-purple-300 flex justify-center">
+            <a href="#" className="text-purple-700 hover:font-semibold flex items-center px-8 hover:px-7 ">
+              My Itenaries
+            </a>
+          </li>
+          <li className="py-3 border-b border-purple-300 flex justify-center">
+            <a href="#" className="text-purple-700 hover:font-semibold flex items-center px-20 hover:px-[78px]">
+              Saved Itenaries
+            </a>
+          </li>
+          <li className="py-3 border-b border-purple-300 flex justify-center">
+            <a href="#" className="text-purple-700 hover:font-semibold flex items-center px-8 hover:px-7">
+              My Wishlist
+            </a>
+          </li>
+          <li className="py-3 flex justify-center">
+            <a href="#" className="text-purple-700 hover:font-semibold flex items-center px-8 hover:px-7">
+              Blogs
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
+
---
+++ frontend/src/pages/DashBoard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import DashNavbar from '../components/DashNavbar';
+import Dash_sidebaar from '../components/Dash_sidebaar';
+const DashBoard = () => {
+  return (
+    <>
+    <div>
+      <DashNavbar/>
+      <Dash_sidebaar/>
+    </div>
+    </>
+  );
+}
+
+export default DashBoard;
+
