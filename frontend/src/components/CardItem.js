ad43c7998da255c071280063ec98cf8421874fd8
sugam <ssugamlm10@gmail.com>
2024-04-25 17:24:38 +0530

    places and cities

---
+++ frontend/src/components/CardItem.js
@@ -0,0 +1,29 @@
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+  Button,
+} from "@material-tailwind/react";
+import { Link } from "react-router-dom";
+
+export function CardItem(props) {
+  return (
+    <Card className="mt-6 w-96 m-10">
+      <Link to={props.path}>
+        <CardHeader color="blue-gray" className="relative h-56">
+          <img src={`images/${props.src}`} alt="card-image" />
+        </CardHeader>
+        <CardBody>
+          <Typography>{props.text}</Typography>
+        </CardBody>
+        {/* <CardFooter className="pt-0">
+          <Button>Read More</Button>
+        </CardFooter> */}
+      </Link>
+    </Card>
+  );
+}
+
+export default CardItem;
