79d6d9549ed5bdb2437cdee9b641c78fdca9caa7
arpit2804 <guptaji1004@gmail.com>
2024-04-25 22:33:15 +0530

    blogs route creation and blog pages creation

--- frontend/src/components/Dashboard/Myitenaries.js
+++ frontend/src/components/Dashboard/Myitenaries.js
@@ -30,7 +30,7 @@ const Myitenaries = () => {
         <p class="mb-3 font-normal text-white ">Last Modified: 1 Sep</p>
         <p class="mb-3 font-normal text-white "></p>
         <a href="#" class="inline-flex items-center px-3 py-2 mb-4 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800  focus:outline-none">
-            Read more
+            View more
              <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
             </svg>
@@ -54,7 +54,7 @@ const Myitenaries = () => {
         <p class="mb-3 font-normal text-white ">Last Modified: 1 Sep</p>
         <p class="mb-3 font-normal text-white "></p>
         <a href="#" class="inline-flex items-center px-3 py-2 mb-4 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800  focus:outline-none">
-            Read more
+            View more
              <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
             </svg>
@@ -78,7 +78,7 @@ const Myitenaries = () => {
         <p class="mb-3 font-normal text-white ">Last Modified: 1 Sep</p>
         <p class="mb-3 font-normal text-white "></p>
         <a href="#" class="inline-flex items-center px-3 py-2 mb-4 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800  focus:outline-none">
-            Read more
+            view more
              <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
             </svg>
@@ -103,7 +103,7 @@ const Myitenaries = () => {
         <p class="mb-3 font-normal text-white ">Last Modified: 1 Sep</p>
         <p class="mb-3 font-normal text-white "></p>
         <a href="#" class="inline-flex items-center px-3 py-2 mb-4 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800  focus:outline-none">
-            Read more
+            View more
              <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
             </svg>
--- frontend/src/pages/App.js
+++ frontend/src/pages/App.js
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DashBoard from "./DashBoard";
 import Register from "./Register";
 import Login from "./Login";
+import Blog from "./Blog";
+import OpenBlog from "./OpenBlog";
+import NewBlog from "./NewBlog";
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
         <Route path="/dashboard" element={<DashBoard />}></Route>
         <Route path="/register" element={<Register />}></Route>
         <Route path="/login" element={<Login />}></Route>
+        <Route path="/blogs" element={<Blog />}></Route>
+        <Route path="/blog" element={<OpenBlog />}></Route>
+        <Route path="/newBlog" element={<NewBlog/>}></Route>
       </Routes>
     </BrowserRouter>
   );
