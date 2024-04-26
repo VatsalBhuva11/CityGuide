df996a29552c8a1a01830f8b7e0c59c1815e8ca5
Vatsal Bhuva <vatsalbhuva11@gmail.com>
2024-04-26 10:52:28 +0530

    fix merge conflicts

--- frontend/src/pages/App.js
+++ frontend/src/pages/App.js
@@ -13,25 +13,25 @@ import Blog3 from "./Blog3";
 import Blog4 from "./Blog4";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/dashboard" element={<DashBoard />}></Route>
-        <Route path="/register" element={<Register />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/places" element={<Places />} />
-        <Route path="/city" element={<City />} />
-        <Route path="/blogs" element={<Blog />}></Route>
-        <Route path="/blog" element={<OpenBlog />}></Route>
-        <Route path="/blog/1" element={<Blog1 />}></Route>
-        <Route path="/blog/2" element={<Blog2 />}></Route>
-        <Route path="/blog/3" element={<Blog3 />}></Route>
-        <Route path="/blog/4" element={<Blog4 />}></Route>
-        <Route path="/newBlog" element={<NewBlog/>}></Route>
-        <Route path="/user/itenary" ></Route>
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/dashboard" element={<DashBoard />}></Route>
+                <Route path="/register" element={<Register />}></Route>
+                <Route path="/login" element={<Login />}></Route>
+                <Route path="/places" element={<Places />} />
+                <Route path="/city" element={<City />} />
+                <Route path="/blogs" element={<Blog />}></Route>
+                <Route path="/blog" element={<OpenBlog />}></Route>
+                <Route path="/blog/1" element={<Blog1 />}></Route>
+                <Route path="/blog/2" element={<Blog2 />}></Route>
+                <Route path="/blog/3" element={<Blog3 />}></Route>
+                <Route path="/blog/4" element={<Blog4 />}></Route>
+                <Route path="/newBlog" element={<NewBlog />}></Route>
+                <Route path="/user/itenary"></Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- frontend/src/pages/Itenary.js
+++ frontend/src/pages/Itenary.js
@@ -13,26 +13,59 @@ const Itenary = () => {
 
     function checkCityClicked(e) {
         if (e.target.value === "Add a city") return;
-        console.log(e.target.value);
-        setCitySelected(e.target.value);
-    }
+        setCitySelected(e.target.value); //mark current city
+        let newItem = {};
+        newItem[e.target.value] = []; //new city
 
-    function addItem() {
-        if (citySelected && item[citySelected].length > 0) {
-            setItenary([...itenary, item]);
-            setCitySelected("");
-            setItem({});
+        setItem(newItem);
+        console.log("item: ", newItem);
+        console.log("itenary length: ", itenary.length);
+        console.log("cityselected array: ", newItem[citySelected]);
+
+        let iteLen = itenary.length;
+        if (iteLen > 0 && Object.values(itenary[iteLen - 1])[0].length === 0) {
+            let newItenary = itenary.slice(0, iteLen - 1);
+            if (
+                iteLen > 1 &&
+                Object.keys(itenary[iteLen - 2])[0] === e.target.value
+            ) {
+                setItem(itenary[iteLen - 2]);
+                setItenary(newItenary);
+            } else {
+                newItenary.push(newItem);
+                setItenary(newItenary);
+                console.log("itenary from if: ", newItenary);
+            }
+        } else {
+            setItenary([...itenary, newItem]);
+            console.log("itenary from else: ", [...itenary, newItem]);
         }
     }
 
+    function addPlace(e) {
+        if (citySelected?.length === 0 || e.target.value === "Add a place")
+            return;
+        let newItem = Object.assign({}, item);
+        newItem[citySelected].push(e.target.value);
+
+        console.log("item: ", newItem);
+        let newItenary = itenary.slice(0, -1);
+        newItenary.push(newItem);
+        setItenary(newItenary);
+        console.log("itenary: ", newItenary);
+    }
+
+    function addItem() {
+        return;
+    }
+
     return (
-        <div className="bg-gray-900">
+        <div className="bg-gray-900 text-white">
             <h1 className="text-2xl text-white text-center p-0 m-0">
                 Create a new Itenary!
             </h1>
             <div className="relative bottom-10 h-screen flex flex-col justify-center items-center w-1/2 m-auto">
                 <div className="h-[80%] w-full border-gray-700 border-2 rounded-lg bg-gray-600">
-                    <div className="flex justify-center items-center"></div>
                     <div className="flex justify-center items-center w-full">
                         <select
                             onChange={(e) => checkCityClicked(e)}
@@ -48,7 +81,10 @@ const Itenary = () => {
                             })}
                         </select>
                         {citySelected.length > 0 && (
-                            <select className="w-full text-center text-white p-2 m-2 rounded-lg bg-slate-700">
+                            <select
+                                onChange={(e) => addPlace(e)}
+                                className="w-full text-center text-white p-2 m-2 rounded-lg bg-slate-700"
+                            >
                                 <option>Add a place</option>
                                 {places[citySelected].map((place, index) => {
                                     return (
@@ -60,9 +96,34 @@ const Itenary = () => {
                             </select>
                         )}
                     </div>
-                    <div className="flex justify-center items-center">
-                        <button onClick={addItem}>Add</button>
-                        <button onClick={finishItem}>Finish</button>
+                    <div className="flex justify-center gap-2 items-center">
+                        <button
+                            onClick={addItem}
+                            className="p-2 bg-slate-400 hover:bg-slate-200 duration-150 rounded-xl cursor-pointer"
+                        >
+                            Finish Itenary
+                        </button>
+                    </div>
+                    <div className="flex flex-col justify-center items-center">
+                        {itenary.length > 0 &&
+                            itenary.map((itenaryItem) => {
+                                return (
+                                    <div className="flex flex-col w-full">
+                                        <h1 className="text-xl flex w-full justify-start items-center">
+                                            {Object.keys(itenaryItem)[0]}
+                                        </h1>
+                                        {itenaryItem[
+                                            Object.keys(itenaryItem)[0]
+                                        ].map((itenaryPlace) => {
+                                            return (
+                                                <p className="flex w-full justify-center items-center">
+                                                    {itenaryPlace}
+                                                </p>
+                                            );
+                                        })}
+                                    </div>
+                                );
+                            })}
                     </div>
                 </div>
             </div>
