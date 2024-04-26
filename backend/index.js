954ef3740d3fa09c596cf71339a1e2453b8f542d
Vatsal Bhuva <vatsalbhuva11@gmail.com>
2024-04-26 13:42:43 +0530

    chatbot working

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,85 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import router from "./routes/index.js"
+import router from "./routes/index.js";
 import morgan from "morgan";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import http from "http";
+import { pipeline } from "stream";
+import { Server } from "socket.io";
 
 dotenv.config();
 
 const app = express();
+const server = http.createServer(app);
+const wss = new Server(server, { cors: { origin: "*" } });
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+
 app.get("/", (req, res) => {
     res.status(200).json({ message: "OK" });
 });
-app.use("/api",router);
-const PORT = process.env.PORT || 7001;
-app.listen(PORT, function () {
+app.use("/api", router);
+const PORT = process.env.PORT;
+
+wss.on("connection", (socket) => {
+    console.log("a user connected");
+    socket.on("message", async function (prompt) {
+        console.log("socket entered prompt: ", prompt);
+        const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+        try {
+            const result = await model.generateContentStream(prompt);
+            console.log("Going to run");
+            for await (const chunk of result.stream) {
+                const chunkText = await chunk.text();
+                console.log(chunkText);
+                socket.emit(
+                    "chat response",
+                    JSON.stringify({ text: chunkText })
+                );
+            }
+        } catch (error) {
+            console.error("Error while streaming content:", error);
+            socket.send("chat error", JSON.stringify({ error: error.message }));
+        }
+
+        console.log("========");
+        console.log("response done");
+        console.log("========");
+    });
+    socket.on("close", () => console.log("Client disconnected"));
+});
+
+server.listen(PORT, function () {
     console.log(`Server is running on port ${PORT}! 🚀`);
 });
+
+// wss.on("connection", function connection(ws) {
+//     console.log("A new client connected!");
+//     ws.on("message", async function incoming(message) {
+//         console.log("Received: %s", message);
+
+//         const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+//         try {
+//             const result = await model.generateContentStream(message);
+//             console.log("Going to run");
+//             for await (const chunk of result.stream) {
+//                 const chunkText = await chunk.text();
+//                 console.log(chunkText);
+//                 ws.send(JSON.stringify({ text: chunkText }));
+//             }
+//         } catch (error) {
+//             console.error("Error while streaming content:", error);
+//             ws.send(JSON.stringify({ error: error.message }));
+//         }
+//     });
+
+//     ws.on("close", () => console.log("Client disconnected"));
+// });
--- frontend/src/pages/City.js
+++ frontend/src/pages/City.js
@@ -3,16 +3,20 @@ import CardItem from "../components/CardItem";
 import cityData from "../components/cityData.json";
 
 const City = () => {
-  return (
-    <div className="bg-gray-900">
-      <h1 className="text-6xl text-center my-5 text-red-950">Cities</h1>
-      <div className="flex flex-row flex-wrap basis-1">
-        {cityData.map((value) => (
-          <CardItem src={value.src} text={value.text} path={value.path} />
-        ))}
-      </div>
-    </ div>
-  );
+    return (
+        <div className="bg-gray-900">
+            <h1 className="text-6xl text-center my-5 text-red-950">Cities</h1>
+            <div className="flex flex-row flex-wrap basis-1">
+                {cityData.map((value) => (
+                    <CardItem
+                        src={value.src}
+                        text={value.text}
+                        path={value.path}
+                    />
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default City;
